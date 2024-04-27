5f8a4da5eb17ba150642ffe9bb2e5de11f855f09
mahade <mahade.adib45@gmail.com>
2024-04-27 09:55:54 +0600

    login page added

--- src/Routes/Route.jsx
+++ src/Routes/Route.jsx
@@ -1,20 +1,25 @@
-import {createBrowserRouter} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Root from "../Layout/Root";
 import NotFoundPage from "../Pages/NotFoundPage";
 import HomePage from "../Pages/Home";
+import LoginPage from "../Pages/LoginPage";
 
 const Route = createBrowserRouter([
     {
-      path: "/",
-      element: <Root/>,
-      errorElement: <NotFoundPage></NotFoundPage>,
-      children: [
-        {
-           path:"/",
-           element: <HomePage/>
-        },
-      ]
+        path: "/",
+        element: <Root />,
+        errorElement: <NotFoundPage></NotFoundPage>,
+        children: [
+            {
+                path: "/",
+                element: <HomePage />
+            },
+        ]
     },
-  ]);
+    {
+        path: "/signIn",
+        element: <LoginPage />,
+    },
+]);
 
 export default Route;
