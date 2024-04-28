425f7e256a8854431bb0470875cf79d73f31b38f
mahade <mahade.adib45@gmail.com>
2024-04-28 17:24:48 +0600

    view details page done

--- src/Pages/ArtDetails.jsx
+++ src/Pages/ArtDetails.jsx
@@ -1,9 +1,37 @@
-
+import { Link, useLoaderData, useParams } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+import { useEffect, useState } from "react";
+import { baseURL } from "../utilitis/url";
+import toast from "react-hot-toast";
+import Spinner from "../Components/Spinner";
 
 const ArtDetails = () => {
+    const { id } = useParams();
+    const [item, setItem] = useState([]);
+    // console.log(user);
+    useEffect(() => {
+        fetch(`http://localhost:5000/products/${id}`)
+            .then((res) => res.json())
+            .then((data) => {
+                setItem(data);
+            });
+    });
     return (
-        <div>
-            <h2>hello</h2>
+        <div className="gadgetContainer mt-8">
+            <div className="card lg:card-side bg-base-100 shadow-xl">
+                <figure><img src={item.image} alt="Movie" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">Name: {item.name}</h2>
+                    <div className="text-white mb-1">
+                        <p>subcategory_Name: {item.subcategory_Name}</p>
+                        <p>customization: {item.customization}</p>
+                        <p>shortDescription: {item.shortDescription}</p>
+                        <p>processing_time: {item.processing_time}</p>
+                        <p>stockStatus: {item.stockStatus}</p>
+                        <p>item_name: {item.item_name}</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
